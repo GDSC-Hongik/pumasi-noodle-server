310c8a7bff36c0709a257f5b46ebfe0671e9ddaa
kckc0608 <kckc0608@gmail.com>
2024-01-10 22:03:54 +0900

    fix: 특정 맡기 데이터를 조회하는 API URL 수정
    GET /care 형태에서 GET /care/id 형태의 URL 을 처리하도록 API를 수정

--- pumasi/care/urls.py
+++ pumasi/care/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import care_list, care_detail
+from .views import care_list, care_detail, my_care
 
 urlpatterns = [
-    path('', care_detail),
+    path('', my_care),
     path('list', care_list),
+    path('<int:pk>', care_detail),
 ]
--- pumasi/care/views.py
+++ pumasi/care/views.py
@@ -8,12 +8,16 @@ def care_list(request):
     print("care list")
     return Response({}, status=200)
 
-@api_view(['GET', 'POST', 'PATCH'])
-def care_detail(request):
+
+@api_view(['GET'])
+def care_detail(request, pk):
     if request.method == 'GET':
         print("get a care data")
-        return Response({"method": "get", "from": "care"}, status=200)
+        return Response({"method": "get", "from": "care", "pk": pk}, status=200)
 
+
+@api_view(['POST', 'PATCH'])
+def my_care(request):
     if request.method == 'POST':
         print("get a care data")
         return Response({"method": "post", "from": "care"}, status=200)
