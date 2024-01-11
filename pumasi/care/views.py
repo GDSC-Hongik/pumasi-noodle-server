249f16b46e47fee921866c9b45882f83c7af7e2e
kckc0608 <kckc0608@gmail.com>
2024-01-11 23:29:36 +0900

    feat: GET care/list API 작성
    DB에 직접 접근하는 방식으로 구현하였습니다.

--- pumasi/care/views.py
+++ pumasi/care/views.py
@@ -1,12 +1,23 @@
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from firebase_admin import firestore
+DB = firestore.client()
 
 # Create your views here.
 @api_view(['GET'])
 def care_list(request):
-    print("care list")
-    return Response({}, status=200)
+    """
+    하나의 컬렉션에서 여러개의 문서를 읽어오는 방법 (컬렉션에서 여러 문서 가져오기 파트)
+    => https://firebase.google.com/docs/firestore/query-data/get-data?hl=ko
+    """
+    care_list = []
+    for care in DB.collection("care").stream():
+        id = care.id
+        care = care.to_dict()
+        care["id"] = id
+        care_list.append(care)
+    return Response(care_list, status=status.HTTP_200_OK)
 
 
 @api_view(['GET'])
@@ -20,7 +31,6 @@ def care_detail(request, pk):
 def my_care(request):
     if request.method == 'POST':
         print("get a care data")
-        return Response({"method": "post", "from": "care"}, status=200)
 
     if request.method == 'PATCH':
         print("get a care data")
