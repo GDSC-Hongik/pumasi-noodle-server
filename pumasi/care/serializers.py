e232109064cfcde12ca8b475bb1934d6258fe260
Sanglok99 <contactlogi@gmail.com>
2024-01-20 01:29:44 +0900

    유효성 검사 구현

--- pumasi/care/serializers.py
+++ pumasi/care/serializers.py
@@ -1,4 +1,7 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+from .constants import GENDER_CHOICES
+
 
 class CareSerializer(serializers.Serializer):
     address = serializers.CharField(max_length=200)
@@ -6,6 +9,15 @@ class CareSerializer(serializers.Serializer):
     date = serializers.CharField(max_length=8)
     start_time = serializers.IntegerField(max_value=2400, min_value=0000)
     end_time = serializers.IntegerField(max_value=2400, min_value=0000)
-    gender = serializers.CharField(max_length=200) # choiceField?
-    status = serializers.CharField(max_length=200, default = 'waiting') # choiceField?
+    gender = serializers.ChoiceField(choices=GENDER_CHOICES)  # choiceField로 바꿈
+    status = serializers.CharField(max_length=200, default='waiting')
     email = serializers.CharField(max_length=200)
+
+    def validate_email(self, value):
+        if '@' not in value:
+            raise ValidationError('유효한 이메일 주소가 아닙니다.')
+        return value
+
+    def validate(self, attrs):
+        if attrs['start_time'] > attrs['end_time']:
+            raise ValidationError('시간을 다시 설정하세요.')
