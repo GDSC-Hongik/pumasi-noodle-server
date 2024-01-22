493e237482630650a790101290de824490b5aeaf
kckc0608 <kckc0608@gmail.com>
2024-01-22 20:14:42 +0900

    FIX: rating 필드 추가, id 필드 required 속성 False로 변경
    맡기 데이터를 추가할 때는 id 필드가 없어도 되므로, required 속성을 false로
    변경하였습니다.
    
    시리얼라이저에 rating 필드가 빠져있어 추가하였습니다.

--- pumasi/care/serializers.py
+++ pumasi/care/serializers.py
@@ -13,7 +13,10 @@ class CareSerializer(serializers.Serializer):
     gender = serializers.ChoiceField(choices=GENDER_CHOICES)  # choiceField로 바꿈
     status = serializers.CharField(max_length=200, default='waiting')
     email = serializers.CharField(max_length=200)
-    id = serializers.CharField(max_length=200)
+    rating = serializers.FloatField(max_value=5.0, min_value=0.0, default=0.0)
+    # id 필드는 백엔드에서 직접 넣어주는 값이라, 데이터를 생성할 때는 없어도 되는 값.
+    id = serializers.CharField(max_length=200, required=False)
+
 
     def validate_email(self, value):
         if '@' not in value:
