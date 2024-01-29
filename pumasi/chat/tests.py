afdb530d009187026494f7d4ca233615d26c7e36
kckc0608 <kckc0608@gmail.com>
2024-01-29 23:32:49 +0900

    test: firebase_client.py 의 일부 로직에 대한 테스트 코드 추가
    1. read_all_chat_rooms 메서드의 테스트 코드 추가
    2. check_chat_room_exists 메서드의 테스트 코드 2개 추가

--- pumasi/chat/tests.py
+++ pumasi/chat/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
-
+from .firebase_client import FirebaseClient
 # Create your tests here.
+class ChatFirebaseClientTest(TestCase):
+    def setUp(self) -> None:
+        self.firebase_client = FirebaseClient()
+        self.user_email = "test@example.com"
+
+    def test_read_all_chat_rooms_of_test_user(self):
+        all_rooms = self.firebase_client.read_all_chat_rooms(user_email=self.user_email)
+        self.assertTrue(len(all_rooms) == 1)
+
+    def test_if_chat_doc_exists_checking_function_works1(self):
+        self.assertFalse(self.firebase_client.check_chat_room_exists(chat_room_id="test"))
+
+    def test_if_chat_doc_exists_checking_function_works2(self):
+        self.assertTrue(self.firebase_client.check_chat_room_exists(chat_room_id="4cMFICHurZSxKsRfOitc"))
