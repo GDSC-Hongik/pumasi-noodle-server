677bfb5df2912fdb9b190bacebeb2d3975a1528a
kckc0608 <kckc0608@gmail.com>
2024-01-29 23:38:44 +0900

    feat: GET /chat/list API 구현

--- pumasi/chat/views.py
+++ pumasi/chat/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .firebase_client import FirebaseClient
+from rest_framework import status
 
-# Create your views here.
-def chat_list():
-    pass
+chat_client = FirebaseClient()
+
+
+@api_view(['GET'])
+def chat_list(request):
+    try:
+        user_email = request.user.get("email")
+        if not user_email:
+            raise PermissionError("유저 정보가 존재하지 않으므로 접근할 수 없습니다.")
+
+        chat_rooms = chat_client.read_all_chat_rooms(user_email=user_email)
+        return Response(chat_rooms, status=status.HTTP_200_OK)
+    except Exception as ex:
+        return Response({
+            "error": "예상치 못한 에러가 발생하였습니다.\n" + str(ex)
+        }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 def chat_detail():
     pass
