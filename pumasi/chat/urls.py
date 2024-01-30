4b31512f32b64d7b28987f5ec399082e1ede6d15
kckc0608 <kckc0608@gmail.com>
2024-01-30 21:19:45 +0900

    feat: POST /chat/ API 등록

--- pumasi/chat/urls.py
+++ pumasi/chat/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import chat_list, chat_detail, chat_settings, chat_message
+from .views import chat_list, chat_detail, chat_settings, chat_message, create_chat_room
 
 urlpatterns = [
+    path('', create_chat_room),
     path('list', chat_list),
     path('<str:room_id>', chat_detail),
     path('<str:room_id>/settings', chat_settings),
