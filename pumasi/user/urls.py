b75c38a258b74a1fbdc7e9ff77b1a3c22fda5b36
Sanglok99 <contactlogi@gmail.com>
2024-01-30 21:44:06 +0900

    user/urls.py에 user_list 추가

--- pumasi/user/urls.py
+++ pumasi/user/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from .views import UserPageView, ChildList, ChildDetail, AddData
+from .views import UserPageView, ChildList, ChildDetail, AddData, user_list
 
 urlpatterns = [
     path("addData", AddData),
+    path("", user_list),
     path("<str:pk>", UserPageView),
     path("<str:pk>/child", ChildList),
     path("<str:pk>/child/<int:child_pk>", ChildDetail),
