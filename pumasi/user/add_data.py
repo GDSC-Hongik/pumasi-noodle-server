dfb059d7d1c49c7e3e52bcd65c07f6f7927f5824
Sanglok99 <contactlogi@gmail.com>
2024-02-01 18:55:14 +0900

    child 수정 기능 추가

--- pumasi/user/add_data.py
+++ pumasi/user/add_data.py
@@ -1,14 +1,40 @@
 from firebase_admin import firestore
 from .firebase_client import FirebaseClient
 
-def add_data_to_firestore():
+def add_user_data_to_firestore():
     client = FirebaseClient()
-    # Firestore 데이터
-    data_to_add = {
+
+    # 추가할 유저 데이터
+    user_data_to_add = {
         'name': 'John Doe',
         'address': '서울특별시 마포구 서교동 357-1',
         'point': 70,
         'introduce': '내 아이처럼 소중하게 보살피겠습니다'
     }
 
-    client.create_care("test@example.com", data_to_add)
+    client.create_user("test@example.com", user_data_to_add)
+
+
+def add_child_data_to_firestore():
+    client = FirebaseClient()
+
+    # 추가할 아이 데이터
+    child_data_to_add_1 = {
+        'name': '아이1',
+        'gender': 'm',
+        'age': '6',
+        'blood_type': 'AB',
+        'allergies': '없음',
+        'notes': '낯을 가림'
+    }
+    child_data_to_add_2 = {
+        'name': '아이2',
+        'gender': 'f',
+        'age': '11',
+        'blood_type': 'A',
+        'allergies': '복숭아 알러지',
+        'notes': '애착인형이 있어야 잠을 잠'
+    }
+
+    client.create_child("test@example.com", 1, child_data_to_add_1)
+    client.create_child("test@example.com", 2, child_data_to_add_2)
--- pumasi/user/urls.py
+++ pumasi/user/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from .views import UserPageView, ChildList, ChildDetail, AddData, user_list
+from .views import UserList, UserDetail, ChildList, ChildDetail, AddUserData, AddChildData
 
 urlpatterns = [
-    path("addData", AddData),
-    path("", user_list),
-    path("<str:pk>", UserPageView),
+    path("", UserList),
+    path("<str:pk>", UserDetail),
     path("<str:pk>/child", ChildList),
     path("<str:pk>/child/<int:child_pk>", ChildDetail),
+    path("addUserData", AddUserData),
+    path("addChildData", AddChildData),
 ]
